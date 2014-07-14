427c228d1f4516e916995cf4cc953277841b2ddb
Chain Yu <yujnln@gmail.com>
2014-07-14 21:19:17 +0800

    init Nerdfeed proj

---
+++ Nerdfeed/Nerdfeed/ListViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ListViewController.swift
+//  Nerdfeed
+//
+//  Created by chain on 14-7-14.
+//  Copyright (c) 2014 chain. All rights reserved.
+//
+
+import UIKit
+
+class ListViewController: UITableViewController {
+    
+    override func tableView(tableView: UITableView!, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    
+    override func tableView(tableView: UITableView!, cellForRowAtIndexPath indexPath: NSIndexPath!) -> UITableViewCell! {
+        return nil
+    }
+}
