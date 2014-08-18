de0c5aeaa8b1f10961d981d2146a64de3a6a0d4d
Chain Yu <yujnln@gmail.com>
2014-08-18 23:03:36 +0800

    init proj

---
+++ Swift-play.playground/section-1.swift
@@ -0,0 +1,25 @@
+// Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+
+var myVar = 42.2
+let myLet = 23
+
+println("I have \(myVar) JPY")
+
+// Array and Dictionary
+let emptyArray = [String]()  // or shoppingList = []
+let emptyDictionary = [String: Int]() // or numberCheck = [:]
+
+// Optional
+var optionalVar: Int? = 42 // nil
+if let myInt = optionalVar {
+    println(myInt)
+} else {
+    println("var is nil")
+}
+
+
