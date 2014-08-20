9f09f29ceb34d53df8eceb453c4e96d0decfe98c
Chain Yu <yujnln@gmail.com>
2014-08-20 22:46:33 +0800

    tips: ?? and Truple

--- Swift-play.playground/section-1.swift
+++ Swift-play.playground/section-1.swift
@@ -23,3 +23,40 @@ if let myInt = optionalVar {
 }
 
 
+// @autoclousure
+func logIfTrue(predicate: @autoclosure ()-> Bool) {
+    if predicate() {
+        println("True")
+    }
+}
+
+logIfTrue(2 > 1)
+
+
+// ??
+// func ??<T>(optional: T?, defaultValue: @autoclosure () -> T) -> T
+
+var level: Int? = 2
+let defaultLv = 1
+let currentLv = level ?? defaultLv
+
+// Truple
+func swapValue<T>(inout a: T, inout b: T) {
+    (a, b) = (b, a)
+}
+var testA = 42
+var testB = 23
+swapValue(&testA, &testB)
+
+func returnTruple(#isOK: Bool) -> (Bool, NSError?) {
+    if isOK {
+        return (true, nil)
+    } else {
+        return (false, NSError(domain: "SomeError", code: 404, userInfo: nil))
+    }
+}
+returnTruple(isOK: false)
+
+var num = 42
+println(num)
+println(num.0.0.0.0.0.0.0.0.0.0)
